7948de5e8fe72ed93c91d96dd8f31b80a00ab07c
edinsonsanabria261 <edinsonsanabria261@gmail.com>
2026-09-15 10:43:42 -0400

    Create download_models.py

---
+++ download_models.py
@@ -0,0 +1,4 @@
+from sentence_transformers import SentenceTransformer
+print("Descargando e instalando modelo all-MiniLM-L6-v2 en el servidor...")
+SentenceTransformer("all-MiniLM-L6-v2")
+print("¡Modelo instalado con éxito!")
